bc871dcbc1491740c59aa5c082e68bccdd54d2e4
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2023-09-24 13:06:35 -0300

    feat: new description to swagger

--- src/main/java/erpservice/alexandre/config/SwaggerConfig.java
+++ src/main/java/erpservice/alexandre/config/SwaggerConfig.java
@@ -3,8 +3,12 @@ package erpservice.alexandre.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+// import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+// import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +22,19 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("erpservice.alexandre")) // caminho do pacote
                 .paths(PathSelectors.any()) // caminho dos endpoints
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Protótipo de Sistema Gerencial de Produtos")
+                .description(
+                        "Esta API fornece uma interface para gerenciar produtos, vendas e clientes. A API é destinada a desenvolvedores. A API inclui endpoints para criar, ler, atualizar e excluir produtos, vendas e clientes.")
+                .version("1.0")
+                // .termsOfServiceUrl("https://www.exemplo.com/terms")
+                // .license("MIT")
+                // .licenseUrl("https://www.exemplo.com/license")
                 .build();
     }
 }
