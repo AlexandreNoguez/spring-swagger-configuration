5ff2631628a79a7d49d51ccae15721a1d3240e40
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2023-09-25 22:10:46 -0300

    fix: swagger config

--- src/main/java/erpservice/alexandre/config/SwaggerConfig.java
+++ src/main/java/erpservice/alexandre/config/SwaggerConfig.java
@@ -16,37 +16,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("erpservice.alexandre")) // caminho do pacote
-                .paths(PathSelectors.any()) // caminho dos endpoints
-                .build()
-                .apiInfo(apiInfo());
-    }
+        @Bean
+        public Docket api() {
+                return new Docket(DocumentationType.SWAGGER_2)
+                                .select()
+                                .apis(RequestHandlerSelectors.basePackage("erpservice.alexandre")) // caminho do pacote
+                                .paths(PathSelectors.any()) // caminho dos endpoints
+                                .build()
+                                .apiInfo(apiInfo());
+        }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Protótipo de Sistema Gerencial de Produtos")
-                .description(
-                        "Esta API fornece uma interface para gerenciar produtos, vendas e clientes. A API é destinada a desenvolvedores. A API inclui endpoints para criar, ler, atualizar e excluir produtos, vendas e clientes.")
-                .version("1.0")
-                // .termsOfServiceUrl("https://www.exemplo.com/terms")
-                // .license("MIT")
-                // .licenseUrl("https://www.exemplo.com/license")
-                .build();
-    }
+        private ApiInfo apiInfo() {
+                return new ApiInfoBuilder()
+                                .title("Protótipo de Sistema Gerencial de Produtos")
+                                .description(
+                                                "Esta API fornece uma interface para gerenciar produtos, vendas e clientes. A API é destinada a desenvolvedores. A API inclui endpoints para criar, ler, atualizar e excluir produtos, vendas e clientes.")
+                                .version("1.0")
+                                // .termsOfServiceUrl("https://www.exemplo.com/terms")
+                                // .license("MIT")
+                                // .licenseUrl("https://www.exemplo.com/license")
+                                .build();
+        }
 
-    public ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Protótipo de Sistema Gerencial de Produtos")
-                .description(
-                        "Esta API fornece uma interface para gerenciar produtos, vendas e clientes. A API é destinada a desenvolvedores. A API inclui endpoints para criar, ler, atualizar e excluir produtos, vendas e clientes.")
-                .version("1.0")
-                .termsOfServiceUrl("https://www.exemplo.com/terms")
-                .license("MIT")
-                .licenseUrl("https://www.exemplo.com/license")
-                .build();
-    }
 }
